e052ddf8911d4c9821ae5ea4cb20974618c618ec
Brian Irons <the.brian.irons@gmail.com>
2022-04-16 13:46:35 +0200

    package install and component setup

---
+++ src/components/SettingsModal/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import "./index.scss"
+
+const SettingsModal = () => {
+  const temp = 1
+  return <div className="container">{temp}</div>
+}
+
+export default SettingsModal
