e81990897bdb6da138ea57cc869666053b1c4eb9
Brian Irons <the.brian.irons@gmail.com>
2022-04-16 17:07:04 +0200

    fixed some linting issue

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,15 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
 import BrandIcon from "../icons/BrandIcon"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCog, faHome } from "@fortawesome/free-solid-svg-icons"
+import "./index.scss"
+import Fab from "../Fab"
 
 export const Navigation = () => {
+  const openModal = () => {
+    //console.log("button push")
+  }
+
   return (
     <nav className="navbar mb-5" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <Link className="navbar-item" to="/">
-          <BrandIcon />
-        </Link>
+      <div className="level navbar-brand is-flex is-align-content-center">
+        <div className="level-left">
+          <Link className="navbar-item" to="/">
+            <BrandIcon />
+          </Link>
+        </div>
+        <div className="level-right mt-0 is-flex is-align-content-center">
+          <Fab type="settings" onClick={openModal}></Fab>
+        </div>
       </div>
+      <i className="fa-solid fa-gear"></i>
       <Link className="navbar-item" to="/">
         Home
       </Link>
