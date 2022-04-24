5040a7bfe0191895a9d11ef866d192be1cbac082
Camille Samson <camsamso@amazon.com>
2022-04-24 18:46:16 +0200

    refactor(util file): rekognition utils file

--- src/components/PhotoAnalysis/rekognitionUtils.ts
+++ src/components/PhotoAnalysis/rekognitionUtils.ts
@@ -1,4 +1,30 @@
-import { Label } from "./PhotoAnalysis"
+import { BoundingBox } from "@aws-amplify/predictions"
+import { LabelType } from "./PhotoAnalysis"
+
+export const labelImage = (labelData: LabelType[], imageSrc: string) => {
+  const img = new Image()
+  img.src = imageSrc
+
+  const height = img.height
+  const width = img.width
+  labelData.forEach(label => {
+    if (label.boundingBoxes != []) {
+      label.boundingBoxes.forEach((boundingBox: BoundingBox) => {
+        defineBoundingBox(boundingBox, height, width)
+      })
+    }
+  })
+}
+
+const defineBoundingBox = (
+  boundingBox: BoundingBox,
+  height: number,
+  width: number
+) => {
+  const top = (boundingBox.top ? boundingBox.top : 0) as number
+  const xPosition = top * width
+  return xPosition
+}
 
 export const isValidFileType = (image: File) => {
   if (
