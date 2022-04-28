8510594e7422b392f7c984550b1ccb89d48c8cc3
Brian Irons <the.brian.irons@gmail.com>
2022-04-28 10:59:02 +0200

    fixed issue of modal boarders due to parent container

--- src/components/SettingsModal/SettingsModal.tsx
+++ src/components/SettingsModal/SettingsModal.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react"
+import ReactDOM from "react-dom"
 import "./SettingsModal.scss"
 
 interface Props {
@@ -12,25 +13,32 @@ const SettingsModal: React.FC<Props> = ({ isActive, toggle }) => {
   if (!isActive) {
     return null
   }
-  return (
-    <div className="modal is-active">
-      <div className="modal-background" onClick={() => toggle} />
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <p className="modal-card-title">MODAL</p>
-          <button className="delete" onClick={() => toggle} />
-        </header>
-        <section className="modal-card-body">
-          <div className="content">{isActive}</div>
-        </section>
-        <footer className="modal-card-foot">
-          {/* eslint-disable-next-line no-console */}
-          <button className="button" onClick={toggle}>
-            Cancel
-          </button>
-        </footer>
+
+  return ReactDOM.createPortal(
+    <React.Fragment>
+      <div className="modal is-active">
+        <div className="modal-background" onClick={toggle} />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Accessibility Settings</p>
+            <button className="delete" onClick={toggle} />
+          </header>
+          <section className="modal-card-body">
+            <div className="content">
+              <h1>Font size</h1>
+              <input className="slider is-Large" />
+            </div>
+          </section>
+          <footer className="modal-card-foot">
+            {/* eslint-disable-next-line no-console */}
+            <button className="button" onClick={toggle}>
+              Cancel
+            </button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </React.Fragment>,
+    document.body
   )
 }
 
--- src/pages/test-page.tsx
+++ src/pages/test-page.tsx
@@ -12,14 +12,18 @@ const TestPage = () => {
   }
 
   return (
-    <Layout pageTitle="test page">
-      <h1>This is a test</h1>
-      <SettingsModal
-        isActive={modalActive}
-        toggle={toggleModal}
-      ></SettingsModal>
-      <button onClick={toggleModal}>settings</button>
-    </Layout>
+    <div className="section">
+      <div className="container is-fluid">
+        <Layout pageTitle="test page">
+          <h1>This is a test</h1>
+          <SettingsModal
+            isActive={modalActive}
+            toggle={toggleModal}
+          ></SettingsModal>
+          <button onClick={toggleModal}>settings</button>
+        </Layout>
+      </div>
+    </div>
   )
 }
 
