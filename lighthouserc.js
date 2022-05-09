c3d3d25ff9b26d47d96fa52a3a9b3e25c9fea428
christoff-linde <christoff.linde@gmail.com>
2022-05-09 19:17:27 +0200

    ci: add offline-plugin-app-shell-fallback index to lhci blacklist
    The inclusion of this page in the lighthouse tests caused the tests to fail as the page does not return any interactible content

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,6 +6,9 @@ module.exports = {
     // },
     collect: {
       staticDistDir: "./public",
+      autodiscoverUrlBlocklist: [
+        "offline-plugin-app-shell-fallback/index.html",
+      ],
     },
     upload: {
       target: "temporary-public-storage",
