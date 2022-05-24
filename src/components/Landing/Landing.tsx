87255a213a2941b704331522f9a51ecdcff657c4
Nicole Lou <u18277897@tuks.co.za>
2022-05-24 21:29:31 +0200

    feat: new photo and profile ui

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -2,6 +2,8 @@ import React from "react"
 
 import "./landing.scss"
 import PhotoAnalysis from "../PhotoAnalysis/PhotoAnalysis"
+import { faCamera } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 interface Props {
   title: string
@@ -10,9 +12,31 @@ interface Props {
 
 const Landing: React.FC<Props> = () => {
   const pageData = (
-    <div className="card analysis-card has-text-light">
-      {/* TODO in future we should be using GatsbyImage for dynamic and StaticImage for any fixed/static images */}
-      {/* <div className="card-image">
+    <div className="mainContainer">
+      <div className="rightControls">
+        <div className="controls">
+          <label className="file-label">
+            <input className="file-input" type="file" />
+            <span className="file-cta">
+              <span className="file-label">
+                <FontAwesomeIcon
+                  className="iconCam"
+                  icon={faCamera}
+                  fontSize="18"
+                />
+              </span>
+            </span>
+          </label>
+        </div>
+        <div className="controls">
+          <button className="icon-button"></button>
+        </div>
+      </div>
+      <br />
+
+      <div className="card analysis-card has-text-light">
+        {/* TODO in future we should be using GatsbyImage for dynamic and StaticImage for any fixed/static images */}
+        {/* <div className="card-image">
         <figure className="image is-4by3">
           <img
             src="https://bulma.io/images/placeholders/1280x960.png"
@@ -20,13 +44,13 @@ const Landing: React.FC<Props> = () => {
           />
         </figure>
        </div> */}
-
-      <div className="media-content">
-        <div className="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
-          <div className="media">
-            <div className="block">
-              <div className="content">
-                <PhotoAnalysis />
+        <div className="media-content">
+          <div className="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+            <div className="media">
+              <div className="block">
+                <div className="content">
+                  <PhotoAnalysis />
+                </div>
               </div>
             </div>
           </div>
