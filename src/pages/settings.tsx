df84c387641b521d1f7cdcee13bc040aa2fcb173
Christoff <christoff.linde@gmail.com>
2022-06-22 23:53:06 +0200

    feature: add settings component

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,15 +1,13 @@
 import React from "react"
 
 import Seo from "../components/Seo/Seo"
-import DarkToggle from "../components/Toggle/ModeToggle"
+import Settings from "../components/Settings/Settings"
 
 const SettingsPage = () => {
   return (
     <>
       <Seo title="Settings" />
-      <h1>Hello There</h1>
-
-      <DarkToggle />
+      <Settings />
     </>
   )
 }
