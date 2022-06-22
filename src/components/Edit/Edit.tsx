9d265af7cc5a34c52ad555cb59fce31e22d41fc3
Nicole Lou <u18277897@tuks.co.za>
2022-06-22 20:58:18 +0200

    feat: general ui

--- src/components/Edit/Edit.tsx
+++ src/components/Edit/Edit.tsx
@@ -45,30 +45,34 @@ const Edit: React.FC<Props> = ({ recordData }) => {
                 <div className="card-image">
                   <img src={useGetFilePath(filepath)} alt="Uploaded preview" />
                 </div>
-                <div className="labels">
-                  <p>Click in the boxes below to edit the labels</p>
-                  {labelsArray.map((label: LabelType, index: number) => {
-                    return (
-                      <span key={index}>
-                        <input
-                          className="input"
-                          key={index}
-                          type="text"
-                          value={label.name}
-                          onChange={updateLabel(index)}
-                        />
-                      </span>
-                    )
-                  })}
+                <div className="px-5 my-4">
+                  <p className="has-text-white has-text-weight-medium">
+                    Click in the boxes below to edit the labels
+                  </p>
+                  <div className="grid-sm gap-4">
+                    {labelsArray.map((label: LabelType, index: number) => {
+                      return (
+                        <span key={index}>
+                          <input
+                            className="input"
+                            key={index}
+                            type="text"
+                            value={label.name}
+                            onChange={updateLabel(index)}
+                          />
+                        </span>
+                      )
+                    })}
+                  </div>
                 </div>
-                <div className="is-flex is-align-self-auto">
+                <div className="is-flex is-align-self-auto px-4 my-4 gap-1">
                   <button
                     className="button is-danger save-button is-align-items-center"
                     onClick={UpdateDynamoRecord}
                   >
                     Save
                   </button>
-                  <br />
+
                   <button
                     className="button is-danger is-align-items-center"
                     onClick={deleteItem}
