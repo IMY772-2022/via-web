6f54982ec0b4912be84fb1141655e0d4ef0fd1fe
Christoff <christoff.linde@gmail.com>
2022-06-23 02:02:11 +0200

    feature: add reusable constants for theming

---
+++ src/components/Toggle/constants.ts
@@ -0,0 +1,17 @@
+export enum ColorMode {
+  light = "light",
+  normal = "normal",
+  dark = "dark",
+}
+
+export enum FontWeight {
+  light = "light",
+  normal = "normal",
+  bold = "bold",
+}
+
+export enum FontSize {
+  smaller = "14px",
+  normal = "16px",
+  larger = "20px",
+}
