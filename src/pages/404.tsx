adda67452615a50cfdec848f4ea912b109bd28df
Camille Samson <camsamso@amazon.com>
2022-06-25 19:44:29 +0200

    Add SEO component to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,13 @@
 import * as React from "react"
+import Seo from "../components/Seo/Seo"
 
 const NotFoundPage = () => {
-  return <p> sorry,page not found </p>
+  return (
+    <>
+      <Seo title="Page not found" />
+      <p> Sorry, page not found </p>
+    </>
+  )
 }
 
 export default NotFoundPage
